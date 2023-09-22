c771e485a237545aa30c64fa8ec731ab9af8465c
Arinze <calistusarinzechinweuba@gmail.com>
2023-09-22 10:40:50 +0100

    Add hamburger icon functionality using JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
+// hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const menuItems = document.querySelectorAll(".nav-item")
+const smallMenuContainer = document.querySelector(".small_screen-menu-container")
+const smallMenu = document.querySelector(".small-menu")
+// console.log(hamburger, menu);
+hamburger.addEventListener("click", () => {
+ smallMenuContainer.classList.toggle("active")
+})
+menuItems.forEach(item => {
+    item.addEventListener("click", () =>{
+        smallMenuContainer.classList.remove("active")
+    })
+});
+
+
+
+
 particlesJS("particles-js", {
   particles: {
     number: { value: 100, density: { enable: true, value_area: 800 } },
